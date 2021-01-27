8f8dfb6488e1837d806e252a038e7fdd30195b4d
Sruthi564 <61182684+Sruthi564@users.noreply.github.com>
2021-01-27 11:46:56 +0530

    Update common_payload.py

--- common_payload.py
+++ common_payload.py
@@ -1,13 +1,9 @@
 """
 Copyright (c) 2020 Nutanix Inc. All rights reserved.
 Author: bhawani.singh@nutanix.com
-
 """
 from constant import ENDOR_PC_BUILD_URL
-
-
 class BasePayload(object):
-
     """
     Payload parameters
     """
@@ -18,7 +14,8 @@ class BasePayload(object):
         self.PLUGINS = dict()
         self.AOS_REL_BRANCH = None
         self.PC_REL_BRANCH = None
-        self.POOL_NAME = []
+        self.POOL_CATEGORY = ''
+        self.POOL_TYPE = ''
         self.PC_URL = ENDOR_PC_BUILD_URL
         self.PC_COMMIT_ID = None
         self.NOS_COMMIT_ID = None
@@ -40,12 +37,9 @@ class BasePayload(object):
                           u'stage': u'post_run',
                           u'name': u'EmailPlugin'}]
         self.PRE_RUN = []
-        self.TASK_PRIORITY = 80
+        self.TASK_PRIORITY = 100
         self.RETRY_IMAGING = 2
-
-
 class GlobalPayloadParameters(object):
-
     """
     Pass
     """
@@ -59,20 +53,20 @@ class GlobalPayloadParameters(object):
         self.DEPLOYMENT_TAG = u'max_deployments__20'
         self.THROTTLE_TAG = u'throttle_tests__20'
         self.EMAILS = [u'bhawani.singh@nutanix.com']
-
-
 class MasterPayload(GlobalPayloadParameters, BasePayload):
-
     """
     Master Branch Payload
     """
-
     def __init__(self):
         GlobalPayloadParameters.__init__(self)
         BasePayload.__init__(self)
-        self.POOL_NAME = [u'AHV-REG-NESTED-POOL']
+        self.POOL_TYPE = u'Nested'
+        self.POOL_CATEGORY = u'general'
+        self.POOL_NAME = [
+            u'phx_bigbang_test'
+        ]
+        # self.POOL_NAME = [u'AHV-REG-NESTED-POOL']
         self.payload_map = dict()
-
     def load_payload(self):
         self.payload_map['git'] = self.get_git
         self.payload_map[u'scheduling_options'] = self.get_scheduling_options
@@ -87,7 +81,6 @@ class MasterPayload(GlobalPayloadParameters, BasePayload):
         self.payload_map[u'test_framework'] = None
         self.payload_map[u'nutest_branch'] = self.get_nutest_branch
         self.payload_map[u'skip_commit_id_validation'] = None
-
     @property
     def get_git(self):
         self.GIT = {
@@ -95,25 +88,20 @@ class MasterPayload(GlobalPayloadParameters, BasePayload):
             u'branch': self.nos_branch
         }
         return self.GIT
-
     @property
     def get_tester_tags(self):
         return [self.DEPLOYMENT_TAG, self.THROTTLE_TAG, self.RDM_TAG]
-
     @property
     def get_nutest_branch(self):
-        self.NUTEST_BRANCH = self.nutest_commit
+        self.NUTEST_BRANCH = self.nutest_branch
         return self.NUTEST_BRANCH
-
     @property
     def get_nutest_commit(self):
         return self.NUTEST_COMMIT
-
     @property
     def get_email_ids(self):
         self.EMAILS = self.email_ids
         return self.EMAILS
-
     @property
     def get_jita_plugins(self):
         self.PLUGINS = {
@@ -121,12 +109,10 @@ class MasterPayload(GlobalPayloadParameters, BasePayload):
             u'pre_run': self.pre_run
         }
         return self.PLUGINS
-
     @property
     def get_build_selection(self):
         self.BUILD_SELECTION = self.get_nos_by_commit_id
         return self.BUILD_SELECTION
-
     @property
     def get_nos_by_commit_id(self):
         self.USE_NOS_BY_COMMIT_ID = {
@@ -137,25 +123,23 @@ class MasterPayload(GlobalPayloadParameters, BasePayload):
             u'use_stable_nutest_commit': False
         }
         return self.USE_NOS_BY_COMMIT_ID
-
     @property
     def get_cluster_selection(self):
         self.CLUSTER_SELECTION = {
-            u'pool_name': self.POOL_NAME,
-            u'by_node_pool': True
+            u'global_pool_type': self.POOL_TYPE,
+            u'category': self.POOL_CATEGORY,
+            u'by_global_pool': True,
+            u'pool_name': self.POOL_NAME
         }
         return self.CLUSTER_SELECTION
-
     @property
     def get_nutest_patch_url(self):
         self.NUTEST_PATCH = self.nutest_patch
         return self.NUTEST_PATCH
-
     @property
     def get_nos_url(self):
         self.NOS_URL = self.nos_url
         return self.NOS_URL
-
     @property
     def get_pc_url(self):
         self.PC_URL = dict(PRISM_CENTRAL={
@@ -164,17 +148,14 @@ class MasterPayload(GlobalPayloadParameters, BasePayload):
             }
         })
         return self.PC_URL
-
     @property
     def get_foundation_build_url(self):
         self.FOUNDATION_BUILD_URL = self.foundation_url
         return self.FOUNDATION_BUILD_URL
-
     @property
     def get_hypervisor_url(self):
         self.HYPERVISOR_URL = self.hypervisor_url
         return self.HYPERVISOR_URL
-
     @property
     def get_requested_hardware(self):
         self.REQUESTED_HARDWARE = {
@@ -198,7 +179,6 @@ class MasterPayload(GlobalPayloadParameters, BasePayload):
             }
         }
         return self.REQUESTED_HARDWARE
-
     @property
     def get_scheduling_options(self):
         self.SCHEDULING_OPTIONS = {
@@ -210,8 +190,6 @@ class MasterPayload(GlobalPayloadParameters, BasePayload):
             u'retry_imaging': self.retry_imaging,
             u'check_image_compatibility': True}
         return self.SCHEDULING_OPTIONS
-
-
 # class GOSPayload(MasterPayload):
 #
 #     def __init__(self, **kwargs):
@@ -241,4 +219,3 @@ class MasterPayload(GlobalPayloadParameters, BasePayload):
 #     print(gos.__dict__)
 #     print(gos.get_tester_tags)
 #     print(gos.load_payload())
-
