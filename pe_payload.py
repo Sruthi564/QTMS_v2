a660684949635b28657155979c602837c2a38403
Sruthi564 <61182684+Sruthi564@users.noreply.github.com>
2021-01-27 11:32:01 +0530

    Update pe_payload.py

--- pe_payload.py
+++ pe_payload.py
@@ -1,13 +1,9 @@
 from common_payload import MasterPayload
 from constant import BUILD_NOS_URL, BUILD_HYPERVISOR_URL
-
-
 class PEPayload(MasterPayload):
-
     def __init__(self, **kwargs):
         # GlobalPayloadParameters.__init__(self)
         MasterPayload.__init__(self)
-
         self.nos_branch = kwargs.get("nos_branch")
         self.nos_commit = kwargs.get("nos_commit")
         self.nutest_commit = kwargs.get("nutest_commit", self.NUTEST_COMMIT)
@@ -21,7 +17,7 @@ class PEPayload(MasterPayload):
         self.foundation_url = kwargs.get("foundation_build_url", self.FOUNDATION_BUILD_URL)
         self.hypervisor_url = kwargs.get("hypervisor_url", self.HYPERVISOR_URL)
         self.nos_url = kwargs.get("nos_url", self.NOS_URL)
-
+        self.nutest_branch = kwargs.get("nutest_branch", self.NUTEST_BRANCH)
     def load_payload(self):
         super(PEPayload, self).load_payload()
         return self.payload_map
